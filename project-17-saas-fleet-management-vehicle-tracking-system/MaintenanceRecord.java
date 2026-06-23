509860d0ff5fe31c4c155ad9dcb209b5cf68f606
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 21:50:54 +0300

    Create MaintenanceRecord.java

---
+++ project-17-saas-fleet-management-vehicle-tracking-system/MaintenanceRecord.java
@@ -0,0 +1,12 @@
+package src.models;
+
+public class MaintenanceRecord {
+
+    private int vehicleId;
+    private String description;
+
+    public MaintenanceRecord(int vehicleId, String description) {
+        this.vehicleId = vehicleId;
+        this.description = description;
+    }
+}
