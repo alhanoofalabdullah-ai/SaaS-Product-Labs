b8b86efeb0fd1401095562bb898e39876615fce5
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 21:59:18 +0300

    Create Vendor.java

---
+++ project-18-saas-contract-management-vendor-governance-system/Vendor.java
@@ -0,0 +1,12 @@
+package src.models;
+
+public class Vendor {
+
+    private int id;
+    private String name;
+
+    public Vendor(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
