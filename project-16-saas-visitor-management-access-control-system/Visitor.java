c68759c05f33df6e683bcc631b472af9f613195c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 21:32:41 +0300

    Create Visitor.java

---
+++ project-16-saas-visitor-management-access-control-system/Visitor.java
@@ -0,0 +1,17 @@
+package src.models;
+
+public class Visitor {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Visitor(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
