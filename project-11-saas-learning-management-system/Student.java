81fdcd7e555b99ffcd76beb166367a0e11acfb9b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 13:34:19 +0300

    Create Student.java

---
+++ project-11-saas-learning-management-system/Student.java
@@ -0,0 +1,17 @@
+package src.models;
+
+public class Student {
+
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
