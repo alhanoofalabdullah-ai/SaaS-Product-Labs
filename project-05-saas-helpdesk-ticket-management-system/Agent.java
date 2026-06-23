83e9159270724850ef381a3f0cd9a858cf9e49a4
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 11:54:45 +0300

    Create Agent.java

---
+++ project-05-saas-helpdesk-ticket-management-system/Agent.java
@@ -0,0 +1,18 @@
+package src.models;
+
+public class Agent {
+
+    private int id;
+    private String name;
+
+    public Agent(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+}
