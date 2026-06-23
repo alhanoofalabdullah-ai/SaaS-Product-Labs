24e04f2957535248d5da61233c15bcad46a04bbc
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 22:32:16 +0300

    Create ProjectDocument.java

---
+++ project-20-saas-enterprise-pmis-project-governance-platform/ProjectDocument.java
@@ -0,0 +1,12 @@
+package src.models;
+
+public class ProjectDocument {
+
+    private String title;
+
+    public ProjectDocument(
+            String title) {
+
+        this.title = title;
+    }
+}
